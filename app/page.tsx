5c55efb06438c6eef4115a203b42cea525d35430
Abhinav Kumar <virusfuf@gmail.com>
2023-06-17 12:25:08 +0530

    updates before paginations

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
-import { Hero, SearchBar } from "@/components";
+import { CarCard, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@util";
 
 
 
-export default function Home() {
+export default async function Home() {
+
+  const allCars = await fetchCars();
+  // console.log(allCars)
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden ">
         <Hero/>
@@ -16,10 +21,29 @@ export default function Home() {
           <SearchBar />
 
           <div className='home__filter-container'>
-            {/* <CustomFilter title='fuel' options={fuels} />
-            <CustomFilter title='year' options={yearsOfProduction} /> */}
+            {/* <CustomFilter title='fuel' options={""} />
+            <CustomFilter title='year' options={""} /> */}
           </div>
           </div>
+          {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+
+            {/* <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            /> */}
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
           </div>
        
     </main>
